0e7682cde284355243a124dbd31336a84afc5a46
Cristian Hornedo <officialcristianl@gmail.com>
2020-07-07 14:48:44 -0400

    Html, got only one p tag on navbar to hold the timer data and view highscore lead to new html page. Css, added styling and hide elements that need to be gone for start screen. JS, Added functions to start game button and got a startgame function to call other functions to keep the game going until the timer hits zero.

---
+++ Assets/scripts.js
@@ -0,0 +1,48 @@
+// ! Holding all the vars and DOMs
+const startBtn = document.querySelector("#startBtn");
+const startScreen = document.querySelector(".show");
+const timerEl = document.querySelector("#countdown");
+
+let scoreboard = [{ name: "Bob", highscore: 0 }];
+
+const quizQandA = {
+  questions: ["Whats the '+' operator"],
+  answers: ["1", "2", "3", "4"],
+  correctAnswerInd: 2,
+};
+
+let score = 0;
+
+// ! function to start the timer and have it display on screen.. Count down from 60 seconds
+function timeStarter(event) {
+  event.preventDefault();
+  let timeLeft = 60;
+  timerEl.textContent = "Time: " + timeLeft;
+
+  let timeInterval = setInterval(() => {
+    timerEl.textContent = "Time: " + timeLeft;
+    timeLeft--;
+
+    if (timeLeft === 0) {
+      timerEl.textContent = "0";
+      console.log("Time is done");
+      clearInterval(timeInterval);
+    }
+  }, 1000);
+}
+
+// !
+
+function startGame() {
+  timeStarter(event);
+  // ? display the question and mutliple choice buttons function
+}
+
+// ! Have a event listener for the start game button
+startBtn.addEventListener("click", () => {
+  startScreen.setAttribute("class", "hide");
+  // ? start timer function
+  startGame();
+});
+
+// ? checking answer, going to next question, eventlistener for answer, and a function to update the display
