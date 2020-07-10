f0d591c6615965202114d4881e0b5f5bcf2432ee
Cristian Hornedo <officialcristianl@gmail.com>
2020-07-10 14:22:31 -0400

    For HTML, added 4 button tags for our answers to appear after the user clicks start game and added a value to each button for us to grab later on to compare to the right answer on javscript. CSS moved the hide class to the bottom of the css. JS we changed the object to have a method (object inside of the object) for each question to hold and got a question to appear on the h1 tag

--- Assets/scripts.js
+++ Assets/scripts.js
@@ -2,13 +2,27 @@
 const startBtn = document.querySelector("#startBtn");
 const startScreen = document.querySelector(".show");
 const timerEl = document.querySelector("#countdown");
+const headerTag = document.querySelector("#startHead");
 
 let scoreboard = [{ name: "Bob", highscore: 0 }];
 
 const quizQandA = {
-  questions: ["Whats the '+' operator"],
-  answers: ["1", "2", "3", "4"],
-  correctAnswerInd: 2,
+  question1: {
+    question: "Whats the '+' operator?",
+    correctAnswerInd: 2,
+  },
+  question2: {
+    question: "what is my name?",
+    correctAnswerInd: 3,
+  },
+  question3: {
+    question: "what symbol do we use for jQuery?",
+    correctAnswerInd: 4,
+  },
+  question4: {
+    question: "What year was javascript made?",
+    correctAnswerInd: 1,
+  },
 };
 
 let score = 0;
@@ -17,34 +31,40 @@ let score = 0;
 function timeStarter(event) {
   event.preventDefault();
   let timeLeft = 60;
-  timerEl.textContent = "Time: " + timeLeft;
+  timerEl.textContent = timeLeft;
 
   let timeInterval = setInterval(() => {
-    timerEl.textContent = "Time: " + timeLeft;
+    timerEl.textContent = timeLeft;
     timeLeft--;
 
     if (timeLeft === 0) {
       timerEl.textContent = "0";
       console.log("Time is done");
       clearInterval(timeInterval);
+      endGame();
     }
   }, 1000);
 }
 
-// ! updateDisplay to show the game
-function updateDisplay(questionsObj) {
-  console.log(questionsObj.questions[0]);
+// * updateDisplay to show the game
+function updateDisplay(questions) {
+  headerTag.textContent = questions.question1.question;
 }
 
+// * The start and end game functions
 function startGame() {
   timeStarter(event);
   // ? display the question and mutliple choice buttons function
   updateDisplay(quizQandA);
 }
 
+function endGame() {
+  headerTag.textContent = "Game Over";
+}
+
 // ! Have a event listener for the start game button
 startBtn.addEventListener("click", () => {
-  startScreen.setAttribute("class", "hide");
+  startBtn.setAttribute("class", "hide");
   // ? start timer function
   startGame();
 });
