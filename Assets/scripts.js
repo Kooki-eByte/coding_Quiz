c225724e14e58a2b2f8d04f8e255ba3c8b415b8e
Cristian Hornedo <officialcristianl@gmail.com>
2020-07-12 11:05:53 -0400

    HTML, Modified the value for each ans button from numbers to alpabetical. JS, gave it a proper array that holds many objects inside for each question and choices and answer for the choices.

--- Assets/scripts.js
+++ Assets/scripts.js
@@ -3,31 +3,36 @@ const startBtn = document.querySelector("#startBtn");
 const startScreen = document.querySelector(".show");
 const timerEl = document.querySelector("#countdown");
 const headerTag = document.querySelector("#startHead");
+const mutlipleChoiceAnswers = document.querySelector("#startPara");
 const ansButtonDiv = document.querySelector(".answerbtns");
 const answerBtnArr = document.querySelectorAll("#ansBtnId");
-
-let scoreboard = [{ name: "Bob", highscore: 0 }];
-
-const quizQandA = {
-  question1: {
-    question: "Whats the '+' operator?",
-    correctAnswerInd: 2,
-  },
-  question2: {
-    question: "what is my name?",
-    correctAnswerInd: 3,
-  },
-  question3: {
-    question: "what symbol do we use for jQuery?",
-    correctAnswerInd: 4,
-  },
-  question4: {
-    question: "What year was javascript made?",
-    correctAnswerInd: 1,
-  },
-};
+const paraDiv = document.querySelector(".paraDiv");
 
 let score = 0;
+let scoreboard = [{ name: "Bob", highscore: 0 }];
+
+const quizQandA = [
+  {
+    question: "Question 1?",
+    choices: "A. ans1\nB. ans2\nC. ans3\nD. ans4",
+    answer: "A",
+  },
+  {
+    question: "Question 2?",
+    choices: "A. ans1\nB. ans2\nC. ans3\nD. ans4",
+    answer: "B",
+  },
+  {
+    question: "Question 3?",
+    choices: "A. ans1\n\n\nB. ans2\nC. ans3\nD. ans4",
+    answer: "C",
+  },
+  {
+    question: "Question 4?",
+    choices: "A. ans1\n\n\nB. ans2\nC. ans3\nD. ans4",
+    answer: "D",
+  },
+];
 
 // ! function to start the timer and have it display on screen.. Count down from 60 seconds
 function timeStarter(event) {
@@ -49,24 +54,31 @@ function timeStarter(event) {
 }
 
 // * updateDisplay to show the game
-function updateDisplay({ question }) {
-  headerTag.textContent = question;
+function updateDisplay(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    // Put question on the header
+    headerTag.textContent = arr[i].question;
+    mutlipleChoiceAnswers.textContent = arr[i].choices;
+    // Run function to return the value of the answer given -- This will also pop a correct or not in the DOM
+    let response = checkAnswer();
+  }
 }
+
+function checkAnswer() {}
 
 // * The start and end game functions
 function startGame() {
   timeStarter(event);
   ansButtonDiv.classList.replace("hide", "show");
-
+  updateDisplay(quizQandA);
   // ? display the question and mutliple choice buttons function
-  updateDisplay(quizQandA.question1);
 }
 
 function endGame() {
   headerTag.textContent = "Game Over";
 }
 
-// ! Have a event listener for the start game button
+// Have a event listener for the start game button
 startBtn.addEventListener("click", () => {
   startBtn.setAttribute("class", "hide");
   // ? start timer function
@@ -78,5 +90,3 @@ ansButtonDiv.addEventListener("click", (event) => {
   let value = event.target.value;
   console.log(value);
 });
-
-// ? checking answer, going to next question, eventlistener for answer, and a function to update the display
