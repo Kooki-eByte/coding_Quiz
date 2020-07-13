36035b5846f9b55ab118ee33bb3a1a87b9f81d6c
Cristian Hornedo <officialcristianl@gmail.com>
2020-07-12 23:48:33 -0400

    Fixed issue with answer buttons not going to next question adn the entire logic of it all, Now moving onto what to do after the game ends and store the score unot a local storage and stay on a scoreboard

--- Assets/scripts.js
+++ Assets/scripts.js
@@ -7,34 +7,37 @@ const mutlipleChoiceAnswers = document.querySelector("#startPara");
 const ansButtonDiv = document.querySelector(".answerbtns");
 const answerBtnArr = document.querySelectorAll("#ansBtnId");
 const paraDiv = document.querySelector(".paraDiv");
+const isCorrect = document.querySelector("#right-or-not");
 
 let score = 0;
 let scoreboard = [{ name: "Bob", highscore: 0 }];
+let questionCounter = 0;
 
 const quizQandA = [
   {
-    question: "Question 1?",
-    choices: "A. ans1\nB. ans2\nC. ans3\nD. ans4",
+    question: "what does RGB stand for?",
+    choices:
+      "(A)Red, Green, Blue (B)Red, Green, Black (C)Rose, Gold, Brown (D)I dont even know lol",
     answer: "A",
   },
   {
     question: "Question 2?",
-    choices: "A. ans1\nB. ans2\nC. ans3\nD. ans4",
+    choices: "A. ans1   B. ans2     C. ans3     D. ans4",
     answer: "B",
   },
   {
     question: "Question 3?",
-    choices: "A. ans1\n\n\nB. ans2\nC. ans3\nD. ans4",
+    choices: "A. ans1   B. ans2     C. ans3     D. ans4",
     answer: "C",
   },
   {
     question: "Question 4?",
-    choices: "A. ans1\n\n\nB. ans2\nC. ans3\nD. ans4",
+    choices: "A. ans1   B. ans2     C. ans3     D. ans4",
     answer: "D",
   },
 ];
 
-// ! function to start the timer and have it display on screen.. Count down from 60 seconds
+// function to start the timer and have it display on screen.. Count down from 60 seconds
 function timeStarter(event) {
   event.preventDefault();
   let timeLeft = 60;
@@ -55,27 +58,25 @@ function timeStarter(event) {
 
 // * updateDisplay to show the game
 function updateDisplay(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    // Put question on the header
-    headerTag.textContent = arr[i].question;
-    mutlipleChoiceAnswers.textContent = arr[i].choices;
-    // Run function to return the value of the answer given -- This will also pop a correct or not in the DOM
-    let response = checkAnswer();
-  }
-}
+  // Put question on the header
+  headerTag.textContent = arr[questionCounter].question;
+  mutlipleChoiceAnswers.textContent = arr[questionCounter].choices;
 
-function checkAnswer() {}
+  console.log("DONE!!");
+}
 
 // * The start and end game functions
 function startGame() {
   timeStarter(event);
   ansButtonDiv.classList.replace("hide", "show");
   updateDisplay(quizQandA);
-  // ? display the question and mutliple choice buttons function
 }
 
 function endGame() {
   headerTag.textContent = "Game Over";
+  mutlipleChoiceAnswers.textContent = "Nice Job! you scored : " + score;
+  ansButtonDiv.classList.replace("show", "hide");
+  isCorrect.textContent = "";
 }
 
 // Have a event listener for the start game button
@@ -85,8 +86,22 @@ startBtn.addEventListener("click", () => {
   startGame();
 });
 
-ansButtonDiv.addEventListener("click", (event) => {
-  // Get the value from the buttons
-  let value = event.target.value;
-  console.log(value);
+// listen for a click for the 4 answer buttons & check if the answer was correct or not
+ansButtonDiv.addEventListener("click", function (event) {
+  const selectedAnswer = event.target.value;
+  const answerForQuestion = quizQandA[questionCounter].answer;
+  console.log(selectedAnswer);
+
+  if (selectedAnswer === answerForQuestion) {
+    score += 5;
+    isCorrect.textContent = "Correct!";
+    isCorrect.setAttribute("style", "color: green");
+    questionCounter++;
+    updateDisplay(quizQandA);
+  } else {
+    isCorrect.textContent = "Incorrect!";
+    isCorrect.setAttribute("style", "color: red");
+    questionCounter++;
+    updateDisplay(quizQandA);
+  }
 });
